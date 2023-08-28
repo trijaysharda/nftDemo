43604333bb87d951620e5f6cfab5eb653d42bc96
trijaysharda <136299900+trijaysharda@users.noreply.github.com>
2023-08-28 14:22:02 +0530

    Add files via upload

---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import {Image, Container} from 'semantic-ui-react';
+import './footer.css';
+import logo from '../../assets/polygon-logo.svg'
+
+export default class StickyLayout extends Component {
+  render() {
+    return (
+    <Container>
+      <footer className="footer">
+        <div className="created">
+         
+          
+        </div>
+        
+      </footer>
+    </Container>
+    )
+  }
+}
---
+++ src/config.js
@@ -0,0 +1,4 @@
+
+export const nftmarketaddress = "0x00f55d1B15505c39DBBCE33Cd54F74CC02d1Af7e"
+export const nftaddress = "0x66ed0114B5637464600632baB814579A9CbE4794"
+  
